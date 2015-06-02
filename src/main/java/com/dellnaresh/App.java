c8cc952dd7756702574ecf4eba3d289071eacec9
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-06-02 20:12:19 +1000

    Project for OCP Exam
    Sample codes for OCP Certification exam

---
+++ src/main/java/com/dellnaresh/App.java
@@ -0,0 +1,19 @@
+package com.dellnaresh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Integer i=10;
+        Integer j=11;
+        Integer k=++i;
+        System.out.println(k);
+        System.out.println(j.equals(k));
+
+    }
+
+}
